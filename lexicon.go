a708c3495d1a502e681b14370e2455c1597df001
tesla59 <nishant@heim.id>
2023-06-28 19:27:00 +0530

    lexicon: add Config parameter and default values

--- lexicon.go
+++ lexicon.go
@@ -16,13 +16,28 @@ type Lexicon struct {
 	Words     []string
 }
 
-func NewLexicon() Lexicon {
-	return Lexicon{
+type Config struct {
+	Count     int
+	Separator string
+}
+
+func NewLexicon(config ...*Config) Lexicon {
+	lexicon := Lexicon{
 		Count:     2,
 		Separator: "-",
 		Words:     readDict(),
 	}
+	if len(config) > 0 {
+		if config[0].Count > 0 {
+			lexicon.Count = config[0].Count
+		}
+		if config[0].Separator != "" {
+			lexicon.Separator = config[0].Separator
+		}
+	}
+	return lexicon
 }
+
 
 func (l Lexicon) Generate() string {
 	words := []string{}
